263c30a505fab439e021b3c08038350d9604f5d7
grisumbras <grisumbras@users.noreply.github.com>
2026-02-24 12:09:47 +0000

    Scheduled update

--- recipes/boost-openmethod/all/conanfile.py
+++ recipes/boost-openmethod/all/conanfile.py
@@ -5,7 +5,7 @@ class BoostOpenmethodRecipe(ConanFile):
     name = 'boost-openmethod'
 
     license = 'BSL-1.0'
-    description = '''Open-methods are free-standing functions that work like virtual functions: they select the b​est overrider from a set, depending on the dynamic type of their arguments. This makes it possible to add polymorphic operations to existing classes, without modifying them. They make patterns like Visitor unnecessary.'''
+    description = '''Open-methods are virtual functions that exist outside of classes, as free-standing functions. They make it possible to add polymorphic behavior to existing classes, without modifying them. This implementation supports single and multiple dispatch.'''
     author = 'Jean-Louis Leroy'
     url = 'https://github.com/boostorg/openmethod.git'
     topics = ['Emulation', 'Programming']
